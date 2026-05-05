fcbd892fcba76911e84bcd379dd247827eb5f20e
Rawanah <rym945665@gmail.com>
2026-05-05 13:34:17 +0300

    save changes

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -3,7 +3,7 @@ import { Routes, Route, Navigate, Outlet } from "react-router-dom";
 
 // --- المكونات المشتركة ---
 import Navbar from "../components/Navbar"; 
-import CompanyNavbar from "../components/CompanyNavbar";
+import CompanyNavbar from "../components/CompanyNavbar.jsx";
 
 // --- تسجيل الدخول ---
 import Login from "../pages/login/login"; 
