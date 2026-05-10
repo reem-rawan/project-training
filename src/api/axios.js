d96dfdafc0211fb83e2f180ddb38408003370b22
reem-rawan <rym945665@gmail.com>
2026-05-11 00:00:55 +0300

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://project-training-production.up.railway.app/api",
+  baseURL: "https://project-training-production-5d59.up.railway.app/api",
   headers: {
     "Content-Type": "application/json",
     "Accept": "application/json",
